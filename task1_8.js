cf459a35d493011ad9d31cf3acfd636f89c36cdf
AndrejKost <vi060480kav@gmail.com>
2022-01-15 23:45:30 +0200

    задание 8

--- task1_8.js
+++ task1_8.js
@@ -2,12 +2,17 @@ var arr = [1,2,3,4,6,7];
 let sum = 0;
 
 for (i of arr) {
-     if (i % 2 === 0){
-         if (i > 3) {
-             sum = sum + i;
-         }
-      }
-
- }
-
+    if ((i % 2 === 0) && (i > 3)) {
+        sum = sum + i;
+    }
+}
 console.log('сумма четных в массиве больше 3: ' + sum);
+
+//for (i of arr) {
+  //   if (i % 2 === 0){
+    //     if (i > 3) {
+      //       sum = sum + i;
+        // }
+      //}
+ //}
+//console.log('сумма четных в массиве больше 3: ' + sum);
