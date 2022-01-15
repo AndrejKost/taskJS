6860f22a1f2130afcc33449803c5baa49ff75a72
AndrejKost <vi060480kav@gmail.com>
2022-01-15 21:06:46 +0200

    задание 6

---
+++ task1_6.js
@@ -0,0 +1,13 @@
+var arr = [1,2,3,6];
+var count = 0;
+for (var elem of arr) {
+    count++;
+}
+console.log(`элементов в массиве: ${count}`)
+
+//var arr = [1,2,5,6];
+//for (var count = 0; count < arr.length; count++) {
+//    console.log(`элементов в массиве: ${count}`);
+//    }
+//
+
