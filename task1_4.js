c8370f774b72d2c8b89651f4e8f5f3ef40c9c35d
AndrejKost <vi060480kav@gmail.com>
2022-01-15 19:59:01 +0200

    задание 4

---
+++ task1_4.js
@@ -0,0 +1,14 @@
+var a = 1;
+switch (true) {
+    case (a === 0):
+        console.log( 0 );
+        break;
+    case (a === 1):
+        console.log( 1 );
+        break;
+    case ( a === 2 ||  a === 3 ):
+        console.log( '2,3');
+}
+
+
+
