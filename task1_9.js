f0d9ac332daf8d61035c901ab08204eef7f707f8
AndrejKost <vi060480kav@gmail.com>
2022-01-16 22:28:19 +0200

    задание 9

---
+++ task1_9.js
@@ -0,0 +1,13 @@
+var arr = [1,2,3,4,5,6];
+var endI = arr.length - 1;
+for (var i = 0;  i < endI; i++) {
+    var endJ = endI - i;
+    for (var j = 0;  j < endJ; j++) {
+        if (arr[j] < arr[j + 1]) {
+            var tmp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = tmp;
+        }
+    }
+}
+console.log(arr);
